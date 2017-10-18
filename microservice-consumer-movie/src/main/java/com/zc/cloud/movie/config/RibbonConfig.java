ec20c5918c1c377b5cb04c30f54f61e3ed322d9b
kyn <13656228484@163.com>
2017-10-18 20:05:27 +0800

    加入robbio

--- microservice-consumer-movie/src/main/java/com/zc/cloud/movie/config/RibbonConfig.java
+++ microservice-consumer-movie/src/main/java/com/zc/cloud/movie/config/RibbonConfig.java
@@ -1,12 +1,8 @@
 package com.zc.cloud.movie.config;
 
-import com.netflix.client.config.IClientConfig;
-import com.netflix.loadbalancer.IPing;
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.PingUrl;
 import com.netflix.loadbalancer.RandomRule;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
